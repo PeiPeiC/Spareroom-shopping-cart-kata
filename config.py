6f5382b70e7066adf15dbbbac0fb60dc4de250f7
PeiPeiC <l86426@gmail.com>
2024-09-05 20:42:12 +0100

    docker

--- config.py
+++ config.py
@@ -8,9 +8,3 @@ class Config:
 
     # Database configuration
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
-
-
-class TestingConfig:
-    TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql://test:test@localhost:5432/testdb'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
